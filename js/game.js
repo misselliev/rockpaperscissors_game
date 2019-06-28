91bc6c848e8df3fd36e36aee157387c858a225e4
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-28 12:39:33 -0700

    redoing style and refractoring js

--- js/game.js
+++ js/game.js
@@ -1,95 +1,98 @@
-<script>
+// Setting user inputs
+var rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  round("rock");
+});
 
-    function computerPlay(){
-        //returns randomly rock, paper or scissors
-        let computerChoices = ["rock","paper","scissors"];
-        let randomChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-        return randomChoice;
-    }
+var paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  round("paper");
+});
 
-    
-    function playRound(playerSelection, computerSelection){
-        //play a round of the game and returns winner
-        let result="";
-        let tie ="It's a tie!";
-        
-        switch(computerSelection){
-            case "rock": 
-                if (playerSelection == "scissors"){
-                    result ="You lose, rock beats scissors!";
-                }
-                else if(playerSelection == "paper"){
-                    result ="You win, paper beats rock!";
-                }
-                else if(playerSelection == computerSelection){
-                    result = tie;
-                }
-                else{
-                    result ="Try again!";
-                }
-                break;
+var scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  round("scissors");
+});
 
-            case "paper":
-                if (playerSelection == "rock"){
-                    result = "You loose, paper beats rock!";
-                }
-                else if(playerSelection=="scissors"){
-                    result = "You win, scissors beat paper!";
-                }
-                else if(playerSelection == computerSelection){
-                    result = tie;
-                }
-                else{
-                    result ="Try again!";
-                }
-                break;
+// Setting computer inputs
+function robot() {
+  let computerMove = ["rock", "paper", "scissors"];
+  let randomMove =
+    computerMove[Math.floor(Math.random() * computerMove.length)];
+  return randomMove;
+}
 
-                case "scissors":
-                if(playerSelection == "rock"){
-                    result = "You win! Rock beats Scissors";
-                } 
-                else if(playerSelection == "paper"){
-                    result = "You lose! Scissors beats Paper";
-                } 
-                else if (computerSelection == playerSelection) {
-                    result = draw;
-                } 
-                else{
-                    result = "Try again!";
-                }
-                break;
-        }
-    return result;
-    }
+// Setting up a game round
+function round(playerSelection, computerSelection) {
+  let result = "";
+  let tie = "Oh, it's a tie! Try again";
 
-    function game(){
-        let userScore = 0;
-        let computerScore = 0;
-        let message ="";
-
-        for (var i=1; i<=5; i++){
-            let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-            let computerSelection = computerPlay();
-            result = playRound(playerSelection, computerSelection);
-            console.log("Round "+i +": "+ result);
-        
-        if(result.includes("win")){
-            userScore +=1;
-        }
-        else{
-            computerScore +=1;
-        }
-
-        if(userScore > computerScore){
-            message = "Congrats! You win!";
-        }
-        else{
-            message = "Better luck next time!";
-        }
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection == "scissors") {
+        result = "You lose, rock beats scissors!";
+      } else if (computerSelection == "paper") {
+        result = "You win, paper beats rock!";
+      } else if (computerSelection == playerSelection) {
+        result = tie;
+      } else {
+        result = "Try again!";
       }
-      console.log("You: "+userScore+ "\nComputer: "+computerScore);
-    }
-    
-    game();
+      break;
 
-</script>
+    case "paper":
+      if (computerSelection == "rock") {
+        result = "You loose, paper beats rock!";
+      } else if (computerSelection == "scissors") {
+        result = "You win, scissors beat paper!";
+      } else if (computerSelection == playerSelection) {
+        result = tie;
+      } else {
+        result = "Try again!";
+      }
+      break;
+
+    case "scissors":
+      if (computerSelection == "rock") {
+        result = "You win! Rock beats Scissors";
+      } else if (computerSelection == "paper") {
+        result = "You lose! Scissors beats Paper";
+      } else if (computerSelection == playerSelection) {
+        result = tie;
+      } else {
+        result = "Try again!";
+      }
+      break;
+  }
+  return result;
+}
+
+// Running a game!
+
+function game() {
+  let userScore = 0;
+  let computerScore = 0;
+  let message = "";
+
+  for (var i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+    let computerSelection = computerPlay();
+    result = playRound(playerSelection, computerSelection);
+    console.log("Round " + i + ": " + result);
+
+    if (result.includes("win")) {
+      userScore += 1;
+    } else {
+      computerScore += 1;
+    }
+
+    if (userScore > computerScore) {
+      message = "Congrats! You win!";
+    } else {
+      message = "Better luck next time!";
+    }
+  }
+  console.log("You: " + userScore + "\nComputer: " + computerScore);
+}
+
+game();
